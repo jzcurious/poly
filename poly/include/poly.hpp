223e87450a67595744e51d1ced15620dd13b50ed
jzcurious <paranoid.hpc.io@gmail.com>
2024-09-29 11:29:08 +0700

    add tests

---
+++ poly/include/poly.hpp
@@ -0,0 +1,128 @@
+#ifndef _POLY_HPP_
+#define _POLY_HPP_
+
+#include <stdexcept>
+
+namespace poly {
+
+template <class D>
+concept has_cid = requires(D d) { d.cid; };
+
+using cid_t = long int;
+#define __generate_cid() __LINE__ + 1
+
+#define __base__         template <class... Derived>
+
+#define __implementation__                                                               \
+                                                                                         \
+ public:                                                                                 \
+  static constexpr const cid_t scid = __generate_cid();                                  \
+  const cid_t cid = scid
+
+#define __implementation(_cid)                                                           \
+                                                                                         \
+ public:                                                                                 \
+  static constexpr const cid_t scid = _cid;                                              \
+  const cid_t cid = _cid
+
+#define __polymorphic__                                                                  \
+                                                                                         \
+ public:                                                                                 \
+  static constexpr cid_t scid = __generate_cid();                                        \
+  const cid_t cid = scid;                                                                \
+                                                                                         \
+ private:                                                                                \
+  bool _is_base_inst = false;                                                            \
+  bool _is_derived_inst = true
+
+#define __end_of_method__ static_assert(true, "")
+
+#define __dispatch_case(_method)                                                         \
+  template <typename DerivedT, typename ResT, typename... ArgsT>                         \
+  inline void _call_impl_##_method(ResT* res, ArgsT&... args)                            \
+    requires has_cid<DerivedT>                                                           \
+  {                                                                                      \
+    using B = std::remove_reference<decltype(*this)>::type;                              \
+    using M = decltype(&B::$##_method);                                                  \
+    constexpr bool overrided                                                             \
+        = static_cast<M>(&B::_method) != static_cast<M>(&DerivedT::_method);             \
+    constexpr bool procedural = std::is_same_v<ResT*, void*>;                            \
+    if constexpr (overrided and procedural) {                                            \
+      static_cast<DerivedT*>(this)->_method(args...);                                    \
+    }                                                                                    \
+    if constexpr (overrided and not procedural) {                                        \
+      *res = static_cast<DerivedT*>(this)->_method(args...);                             \
+    }                                                                                    \
+    if constexpr (not overrided and procedural) {                                        \
+      this->$##_method(args...);                                                         \
+    }                                                                                    \
+    if constexpr (not overrided and not procedural) {                                    \
+      *res = this->$##_method(args...);                                                  \
+    }                                                                                    \
+  }                                                                                      \
+  __end_of_method__
+
+#define __dispatch(_method)                                                              \
+                                                                                         \
+ public:                                                                                 \
+  __dispatch_case(_method);                                                              \
+  template <typename... Ts>                                                              \
+  inline auto _method(Ts... args) {                                                      \
+    if (_is_base_inst) {                                                                 \
+      return this->$##_method(args...);                                                  \
+    }                                                                                    \
+    using ResT = decltype(this->$##_method(args...));                                    \
+    if constexpr (not std::is_void_v<ResT>) {                                            \
+      ResT res{};                                                                        \
+      _is_base_inst = not((static_cast<Derived*>(this)->cid == Derived::scid             \
+                                  ? (_call_impl_##_method<Derived>(&res, args...),       \
+                                        _is_derived_inst = true)                         \
+                                  : false)                                               \
+                          || ...);                                                       \
+      if (_is_base_inst) {                                                               \
+        return this->$##_method(args...);                                                \
+      }                                                                                  \
+      return res;                                                                        \
+    } else {                                                                             \
+      _is_base_inst = not((static_cast<Derived*>(this)->cid == Derived::scid             \
+                                  ? (_call_impl_##_method<Derived>((void*)0, args...),   \
+                                        _is_derived_inst = true)                         \
+                                  : false)                                               \
+                          || ...);                                                       \
+      if (_is_base_inst) {                                                               \
+        this->$##_method(args...);                                                       \
+      }                                                                                  \
+    }                                                                                    \
+  }                                                                                      \
+  __end_of_method__
+
+#define PARENS                ()
+
+#define EXPAND(...)           EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...)          EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...)          EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...)          EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...)          __VA_ARGS__
+
+#define FOR_EACH(_macro, ...) __VA_OPT__(EXPAND(FOR_EACH_HELPER(_macro, __VA_ARGS__)))
+
+#define FOR_EACH_HELPER(_macro, _a1, ...)                                                \
+  _macro(_a1) __VA_OPT__(, FOR_EACH_AGAIN PARENS(_macro, __VA_ARGS__))
+
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+
+#define __unused_arg(_a) [[maybe_unused]] _a
+
+#define __abstract_call_msg__                                                            \
+  std::string("Calling the abstract method \"") + __PRETTY_FUNCTION__ + "\" is prohibited"
+
+#define __abstract(_name, _ret, ...)                                                     \
+  [[maybe_unused]] _ret $##_name(FOR_EACH(__unused_arg, __VA_ARGS__)) {                  \
+    throw std::logic_error(__abstract_call_msg__);                                       \
+    return _ret{};                                                                       \
+  }                                                                                      \
+  __end_of_method__
+
+}  // namespace poly
+
+#endif  // _POLY_HPP_
---
+++ tests/src/tests.cpp
@@ -0,0 +1,60 @@
+#include "flat.cpp"
+#include "gtest/gtest.h"
+
+TEST(FlatInhTests, all_implemented) {
+  using namespace flat;
+
+  using Base = Base<ChildA, ChildB, ChildC>;
+
+  auto base = new Base();
+  auto child_a = new ChildA();
+  auto child_b = new ChildB();
+  auto child_c = new ChildC();
+
+  auto child_a_ = static_cast<Base*>(child_a);
+  auto child_b_ = static_cast<Base*>(child_b);
+  auto child_c_ = static_cast<Base*>(child_c);
+
+  EXPECT_NE(base->get_id(), child_a_->get_id());
+  EXPECT_NE(base->get_id(), child_b_->get_id());
+  EXPECT_NE(base->get_id(), child_c_->get_id());
+}
+
+TEST(FlatInhTests, not_impemented_in_derived) {
+  using namespace flat;
+
+  using Base = Base<ChildA, ChildB, ChildC>;
+
+  auto base = new Base();
+  auto child_a = new ChildA();
+  auto child_b = new ChildB();
+  auto child_c = new ChildC();
+
+  auto child_a_ = static_cast<Base*>(child_a);
+  auto child_b_ = static_cast<Base*>(child_b);
+  auto child_c_ = static_cast<Base*>(child_c);
+
+  EXPECT_EQ(base->f1(), child_a_->f1());
+  EXPECT_EQ(base->f1(), child_b_->f1());
+  EXPECT_EQ(base->f1(), child_c_->f1());
+}
+
+TEST(FlatInhTests, abstract) {
+  using namespace flat;
+
+  using Base = Base<ChildA, ChildB, ChildC>;
+
+  auto base = new Base();
+  auto child_a = new ChildA();
+  auto child_b = new ChildB();
+  auto child_c = new ChildC();
+
+  auto child_a_ = static_cast<Base*>(child_a);
+  auto child_b_ = static_cast<Base*>(child_b);
+  auto child_c_ = static_cast<Base*>(child_c);
+
+  EXPECT_EQ(child_a_->f2(2, 2), 4);
+  EXPECT_THROW(base->f2(2, 2), std::logic_error);
+  EXPECT_THROW(child_b_->f2(2, 2), std::logic_error);
+  EXPECT_THROW(child_c_->f2(2, 2), std::logic_error);
+}
